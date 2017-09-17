fb76bf3cdff9f1d5a381cf2c7f7648eb9b156656
estebandalelr <estebandalelr@gmail.com>
2017-09-17 17:30:24 -0500

    Take from backend the tenants

--- buildingadminfront/src/App.js
+++ buildingadminfront/src/App.js
@@ -1,19 +1,42 @@
 import React, { Component } from 'react';
+import Tenants from './Tenants.js';
+
 import logo from './logo.svg';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super (props);
+    this.state = {
+      tenants:[
+      ]
+    };
+  }
+
+  componentDidMount(){
+    fetch('/tenants',{method:'GET',
+     headers:{accept:'application/json'}})
+    .then((res)=>{
+      if(res.ok)
+        return res.json();
+    })
+    .then((tenants) =>{
+      this.setState({
+        tenants:tenants
+      });
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
+
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+
           Hola muuuundoooooo
         </p>
+        <Tenants tenants={this.state.tenants}/>
       </div>
     );
   }
