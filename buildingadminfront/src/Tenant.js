a671d97360dd0bc5f29f5fef3cb14c11fe2876bc
estebandalelr <estebandalelr@gmail.com>
2017-09-17 12:59:14 -0500

    Created the tenant.js file to export each tenant

---
+++ buildingadminfront/src/Tenant.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Tenant extends Component {
+  constructor(props) {
+    super (props);
+  }
+
+
+  render() {
+    return (
+      <div>
+        Tenant
+      </div>
+    );
+  }
+}
+
+export default Tenant;
