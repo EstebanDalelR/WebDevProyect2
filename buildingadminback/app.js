efdeaf4acba29154e8bacc71350436f0427750c2
estebandalelr <estebandalelr@gmail.com>
2017-09-17 17:56:06 -0500

    Correct routing

--- buildingadminback/app.js
+++ buildingadminback/app.js
@@ -20,7 +20,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../buildingadminfront/public')));
+app.use(express.static(path.join(__dirname, '../buildingadminfront/build')));
 
 app.use('/', index);
 // app.use('/workers', workers);
