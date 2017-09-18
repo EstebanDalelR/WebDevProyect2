dcea8c130048116237fcd03df4491e0abb1096e0
estebandalelr <estebandalelr@gmail.com>
2017-09-17 21:31:17 -0500

    added payment with colors and email

--- buildingadminfront/src/Tenant.js
+++ buildingadminfront/src/Tenant.js
@@ -5,12 +5,24 @@ class Tenant extends Component {
     super (props);
   }
 
+  renderTenants(){
+    const payments = this.props.tenant.payments;
+    const listItems = payments.map((payment, index) =>
+       <b key={index} style={{color: payment.paid ? 'green' : 'red'}}>{payment.amount}</b>
+    );
+    return (
+      <td>{listItems}</td>
+    );
+  }
+
+
   render() {
     return (
       <tr>
         <td className="aptNumber">{this.props.tenant.number}</td>
         <td className="tenantName">{this.props.tenant.tenants[0].name}</td>
         <td className="tenantPhone">{this.props.tenant.tenants[0].cel}</td>
+        {this.renderTenants()}
       </tr>
     );
   }
