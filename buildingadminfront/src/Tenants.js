2b6773fe29481b9362fb80698439bbe639b32d62
estebandalelr <estebandalelr@gmail.com>
2017-09-17 21:30:55 -0500

    added email

--- buildingadminfront/src/Tenants.js
+++ buildingadminfront/src/Tenants.js
@@ -1,28 +1,36 @@
 import React, { Component } from 'react';
 import Tenant from './Tenant.js';
+import Month from './Month.js';
 
 class Tenants extends Component {
   constructor(props) {
     super (props);
   }
 
+
   renderTenants(){
     return this.props.tenants.map(
       (t,i)=>{
-      return <Tenant tenant={t} key={i}/>;
+        return <Tenant tenant={t} key={i}/>;
       }
     );
   }
+
+
+
   render() {
     return (
       <div>
         <table>
-          <tr>
-           <th>Apto</th>
-           <th>Nombre</th>
-           <th>Telefono</th>
-         </tr>
-        {this.renderTenants()}
+          <tbody>
+            <tr>
+              <th>Apto</th>
+              <th>Nombre</th>
+              <th>Telefono</th>
+              <th>Correo</th>
+            </tr>
+            {this.renderTenants()}
+          </tbody>
         </table>
       </div>
     );
